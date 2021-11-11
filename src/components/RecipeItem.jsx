c5f944e5c2789b1a8d2f8b4839b69a36d2ec6c8a
Andrew <dandrewbarlow@gmail.com>
2021-11-11 15:44:48 -0700

    boilerplate components

---
+++ src/components/RecipeItem.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RecipeItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
